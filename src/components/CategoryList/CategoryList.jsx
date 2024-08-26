9c46e976bd694ad9d0c632ae6f66c04e9a1fb801
Alif Hossain <alifhossain56782@gmail.com>
2024-08-26 17:38:05 +0600

    Overall done responsive and bugs fixed

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -7,7 +7,7 @@ const CategoryList = () => {
     const [jobData,setJobdata]= useState([]);
     
     useEffect(() => {
-        fetch('/public/categories.json')
+        fetch('categories.json')
         .then(res => res.json())
         .then(data => setJobdata(data))
 
